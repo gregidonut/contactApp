c0fdf59c1bcfd988cf6c8c8bba7be8dfa1814453
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-10-24 22:59:23 +0800

    feat: make the index page even less complicated
    By just having it return a hello world string

--- cmd/web/controller/handlers/pages/index.go
+++ cmd/web/controller/handlers/pages/index.go
@@ -2,30 +2,32 @@ package pages
 
 import (
 	"github.com/gregidonut/contactApp/cmd/web/controller/application"
-	"html/template"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, app *application.Application) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
+	//if r.URL.Path != "/" {
+	//	http.NotFound(w, r)
+	//	return
+	//}
 
-	files := []string{
-		"./ui/html/base.gohtml",
-		"./ui/html/pages/index.gohtml",
-	}
+	//files := []string{
+	//	"./ui/html/base.gohtml",
+	//	"./ui/html/pages/index.gohtml",
+	//}
+	//
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.CatchHandlerErr(w, err, http.StatusInternalServerError)
+	//	return
+	//}
+	//
+	//err = ts.ExecuteTemplate(w, "base", nil)
+	//if err != nil {
+	//	app.CatchHandlerErr(w, err, http.StatusInternalServerError)
+	//	return
+	//}
+	//
 
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.CatchHandlerErr(w, err, http.StatusInternalServerError)
-		return
-	}
-
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.CatchHandlerErr(w, err, http.StatusInternalServerError)
-		return
-	}
+	w.Write([]byte(`<h1>hello world!</h1>`))
 }
