a405646abf989196b6abe03a9248df99fde78012
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-10-24 23:11:07 +0800

    feat: redirect to a "/contacts" endpoint from root
    This way to play around with the redirects

--- cmd/web/controller/handlers/pages/index.go
+++ cmd/web/controller/handlers/pages/index.go
@@ -6,28 +6,7 @@ import (
 )
 
 func Index(w http.ResponseWriter, r *http.Request, app *application.Application) {
-	//if r.URL.Path != "/" {
-	//	http.NotFound(w, r)
-	//	return
-	//}
+	//w.Write([]byte(`<h1>hello world!</h1>`))
 
-	//files := []string{
-	//	"./ui/html/base.gohtml",
-	//	"./ui/html/pages/index.gohtml",
-	//}
-	//
-	//ts, err := template.ParseFiles(files...)
-	//if err != nil {
-	//	app.CatchHandlerErr(w, err, http.StatusInternalServerError)
-	//	return
-	//}
-	//
-	//err = ts.ExecuteTemplate(w, "base", nil)
-	//if err != nil {
-	//	app.CatchHandlerErr(w, err, http.StatusInternalServerError)
-	//	return
-	//}
-	//
-
-	w.Write([]byte(`<h1>hello world!</h1>`))
+	http.Redirect(w, r, "/contacts", http.StatusPermanentRedirect)
 }
